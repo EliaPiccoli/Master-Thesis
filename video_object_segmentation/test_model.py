9748e27d646a2f3970e5d95f2a687083717b5275
EliaPiccoli <eliakoby@live.it>
2022-06-13 16:02:04 +0200

    plot model

--- video_object_segmentation/test_model.py
+++ video_object_segmentation/test_model.py
@@ -1,5 +1,6 @@
 import torch
 from model import VideoObjectSegmentationModel
+import hiddenlayer as hl
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -11,3 +12,12 @@ print(IN.shape)
 
 out = model(IN)
 print("O:", out.shape)
+
+transforms = [ hl.transforms.Prune('Constant') ]
+graph = hl.build_graph(model, IN, transforms=transforms)
+graph.theme = hl.graph.THEMES['blue'].copy()
+graph.save('magic', format='png')
+
+# TODO
+# 1- Create dataset
+# 2- Training cycle
