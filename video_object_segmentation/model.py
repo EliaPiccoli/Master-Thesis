2bd16982e8309906516c10528e0fcb6f1f60440e
EliaPiccoli <eliakoby@live.it>
2022-06-23 19:11:38 +0200

    pong/pacman vos full

--- video_object_segmentation/model.py
+++ video_object_segmentation/model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from kornia.losses import ssim_loss
+import copy
 
 # !! in tf channels is last dimension
 
@@ -17,6 +18,8 @@ class VideoObjectSegmentationModel(nn.Module):
         self.of_reg_inc = 1e-5
         self.K = K
         self.depth = depth
+        self.of = None
+        self.object_masks = None
         self.final_conv_size = 7 * 7 * 64 # check
         self.cnn = nn.Sequential(
             nn.Conv2d(2, 32, 8, stride=4),
@@ -70,6 +73,7 @@ class VideoObjectSegmentationModel(nn.Module):
         # [ BS x K x H x W ]
         m = self.conv_m3(m)
         m = self.sigmoid(m)
+        self.object_masks = m
 
         # Optical Flow
         # [ BS x K x 2 x 1 x 1 ]
@@ -79,19 +83,19 @@ class VideoObjectSegmentationModel(nn.Module):
         m_reshape = torch.unsqueeze(m, 2)
 
         flow_out = ot_reshape * m_reshape
-        reg = ot_reshape * m_reshape
+        self.of = flow_out
 
         # [ BS x 2 x H x W ]
         out = torch.sum(flow_out, 1)
 
-        return out, reg
+        return out
 
-    def compute_loss(self, inp, out, of):
+    def compute_loss(self, inp, out):
         # DSSIM
         out_loss = (1 - ssim_loss(inp, out, 11))/2
 
         # L1 reg for of
-        of_loss_reg = torch.abs(of).mean().mean().mean().mean()
+        of_loss_reg = torch.abs(self.of).mean().mean().mean().mean()
         
         loss = out_loss + self.of_reg_cur * of_loss_reg
         
--- video_object_segmentation/train_model.py
+++ video_object_segmentation/train_model.py
@@ -1,16 +1,23 @@
-from numpy import Inf
+import numpy as np
 import torch
 import wandb
 import os
+import sys
 import torch.nn.functional as F
 from model import VideoObjectSegmentationModel
 from dataset import Dataset
 
-torch.set_num_threads(1)
-device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+if len(sys.argv) < 3:
+    print(f"Usage: python {sys.argv[0]} <env> <gpu-device>")
+    exit()
+env = sys.argv[1]
+gpu = sys.argv[2]
 
-env = "PongNoFrameskip-v4"
-batch_size = 32
+torch.set_num_threads(1)
+device = torch.device(gpu if torch.cuda.is_available() else "cpu")
+
+# env = "PongNoFrameskip-v4"
+batch_size = 16
 H = W = 84
 num_frames = 2
 steps = 250000
@@ -34,38 +41,40 @@ model.to(device)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-best_val_loss = Inf
+best_val_loss = np.Inf
 for i in range(steps):
     model.train()
     optimizer.zero_grad()
     inp = data.get_batch("train").to(device)
-    flow_out, of = model(inp)
+    flow_out = model(inp)
     flow_out = torch.reshape(flow_out, (-1, flow_out.size(2), flow_out.size(3), flow_out.size(1)))
     x0 = torch.unsqueeze(inp[:, 0, :, :], 1)
     x0_ = F.grid_sample(x0, flow_out, align_corners=False)
     x1 = torch.unsqueeze(inp[:, 1, :, :], 1)
-    tr_loss = model.compute_loss(x0_, x1, of)
+    tr_loss = model.compute_loss(x0_, x1)
     tr_loss.backward()
     optimizer.step()
 
     model.eval()
     inp = data.get_batch("val").to(device)
-    flow_out, of = model(inp)
+    flow_out = model(inp)
     flow_out = torch.reshape(flow_out, (-1, flow_out.size(2), flow_out.size(3), flow_out.size(1)))
     x0 = torch.unsqueeze(inp[:, 0, :, :], 1)
     x0_ = F.grid_sample(x0, flow_out, align_corners=False)
     x1 = torch.unsqueeze(inp[:, 1, :, :], 1)
-    val_loss = model.compute_loss(x0_, x1, of)
+    val_loss = model.compute_loss(x0_, x1)
 
     if val_loss < best_val_loss:
+        # print(f"Ep: {i} new best val_loss : {val_loss}")
         torch.save(model.state_dict(), os.path.join(wandb.run.dir, env + '.pt'))
         best_val_loss = val_loss
 
     if i % 100 == 0:
-        print(f"Step: {i} - TLoss: {tr_loss.item()} - VLoss: {val_loss.item()}")
+        print(f"Step: {i} - TLoss: {tr_loss.item()} - VLoss: {val_loss.item()} - BestV: {best_val_loss}")
     wandb.log({
             "train_loss": tr_loss,
             "val_loss": val_loss,
+            "best_val": best_val_loss,
             "reg_par": model.of_reg_cur
         }, step=i)
 
